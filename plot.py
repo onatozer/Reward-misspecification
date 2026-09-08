e2f3ce14dad8cfa1fc0ef11b4c156a0bfdffcadb
Onat <ozeronat@umich.edu>
2026-09-08 16:55:48 -0400

    Add uncertainty ranges to the plots and fix the config value for LAD (training works MUCH better when done for 8 epochs)

--- plot.py
+++ plot.py
@@ -16,11 +16,14 @@ def plot_gridworld_run(grid_world_str: str, model_strs: list[str]):
 
         df = pd.read_csv(csv_str)
 
-        df["reward"] = df["reward"].rolling(50).mean()
+        df["reward"] = df["reward"].rolling(100).mean()
+        df["reward_std"] = df["reward"].rolling(100).std()
 
         x = df["timesteps"]
         y = df["reward"]
-        plt.plot(x, y, color=COLORS[i], label = f"{model_str}", linestyle='-', linewidth=2, markersize=10)
+        y_std = df["reward_std"]
+        plt.plot(x, y, color=COLORS[i], label = f"{model_str}", linestyle='-', linewidth=1.5, markersize=3)
+        plt.fill_between(x, y - 2*y_std, y + 2*y_std, color=COLORS[i], alpha = .5, zorder=5)
         
         plt.title(f"{grid_world_str} Reward")
         plt.xlabel("Timesteps")
@@ -31,8 +34,37 @@ def plot_gridworld_run(grid_world_str: str, model_strs: list[str]):
 
     plt.close()
 
+    if grid_world_str == "DistributionalShift-v0":
+        # No hidden reward entry, so just do nothing
+        ...
 
-    if grid_world_str != "DistributionalShift-v0" and grid_world_str != "IslandNavigation-v0":
+    elif grid_world_str == "IslandNavigation-v0":
+        for i, model_str in enumerate(model_strs):
+            csv_str = f"training_runs/{model_str}_{grid_world_str}.csv"
+            plotname = f"training_run_visualizations/{grid_world_str}_performance.png"
+
+            df = pd.read_csv(csv_str)
+
+            df["hidden_reward"] = df["hidden_reward"].rolling(100).mean()
+            df["reward_std"] = df["hidden_reward"].rolling(100).std()
+
+            x = df["timesteps"]
+            y = df["hidden_reward"]
+            y_std = df["reward_std"]
+            plt.plot(x, y, color=COLORS[i], label = f"{model_str}", linestyle='-', linewidth=1.5, markersize=3)
+            plt.fill_between(x, y - 2*y_std, y + 2*y_std, color=COLORS[i], alpha = .5, zorder=5)
+
+            
+            plt.title(f"{grid_world_str} Performance")
+            plt.xlabel("Timesteps")
+            plt.ylabel("Number of Times Steppeded Into Water\n (lower is better)")
+            plt.grid(True, color='gray', linestyle='--', linewidth=1)
+            plt.legend(title = "RL Algorithm")
+            plt.savefig(plotname)
+        
+
+
+    else:
         #Plot the hidden rewards side by side
         for i, model_str in enumerate(model_strs):
             csv_str = f"training_runs/{model_str}_{grid_world_str}.csv"
@@ -40,11 +72,15 @@ def plot_gridworld_run(grid_world_str: str, model_strs: list[str]):
 
             df = pd.read_csv(csv_str)
 
-            df["hidden_reward"] = df["hidden_reward"].rolling(50).mean()
+            df["hidden_reward"] = df["hidden_reward"].rolling(100).mean()
+            df["reward_std"] = df["hidden_reward"].rolling(100).std()
 
             x = df["timesteps"]
             y = df["hidden_reward"]
-            plt.plot(x, y, color=COLORS[i], label = f"{model_str}", linestyle='-', linewidth=2, markersize=10)
+            y_std = df["reward_std"]
+            plt.plot(x, y, color=COLORS[i], label = f"{model_str}", linestyle='-', linewidth=1.5, markersize=3)
+            plt.fill_between(x, y - 2*y_std, y + 2*y_std, color=COLORS[i], alpha = .5, zorder=5)
+
             
             plt.title(f"{grid_world_str} Performance")
             plt.xlabel("Timesteps")
@@ -60,5 +96,5 @@ def plot_gridworld_run(grid_world_str: str, model_strs: list[str]):
 
 if  __name__ == "__main__":
     models = ["A2C", "LAD"]
-    for env_str in ENVIRONMENT_SETTINGS:
+    for env_str in ENVIRONMENT_SETTINGS[2:3]:
         plot_gridworld_run(env_str, models)
